f2f318569593efb38a73d7aee4830646c34fbf2b
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2025-11-15 12:25:40 +0530

    init commit

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const connectDB = async () => {
 
 connectDB();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
